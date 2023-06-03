5d245e4390e62e7cebd94c2b6933fa49b2a72631
AnnisaDmynt221 <97298883+AnnisaDmynt221@users.noreply.github.com>
2023-06-03 15:02:12 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import navbar from './components/navbar.vue'
+import Respon from './components/Respon.vue'
+</script>
+
+<template>
+  <header>
+    <navbar>
+
+    </navbar>
+  </header>
+
+  <main>
+    <Respon> 
+
+    </Respon>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
